1a8c35585c46b220cb9e7af1e7cf17c1dc1c8f84
Selina Su <phongsu20@gmail.com>
2022-12-01 11:07:54 -0800

    fix the date

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,8 +1,10 @@
+function formatDate(date) {
+    return `${new Date(date).getMonth() + 1}/${new Date(date).getDate()}/${new Date(
+        date
+      ).getFullYear()}`;
+}
+
+
 module.exports = {
-    format_date: (date) => {
-        return date;
-      },
-    format_time: (date) => {
-        return `${new Date(date).getMonth() + 1}/${new Date(date).getDate()}/${new Date(date).getFullYear()}`;
-    },
-};
+    formatDate
+}
